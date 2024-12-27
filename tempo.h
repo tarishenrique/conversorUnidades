cac515fa6c585c8181fe800a9fa86ae9cc28c3f3
Thiago <ts0141632@outlook.com>
2024-12-26 23:59:31 -0300

    Add files via upload
    @taris meus arquivos (desculpe a demora)

---
+++ tempo.h
@@ -0,0 +1,24 @@
+#ifndef TEMPO_H
+#define TEMPO_H
+
+float horas_para_minutos(float horas);
+
+float horas_para_segundos(float horas);
+
+float minutos_para_horas(float minutos);
+
+float minutos_para_segundos(float minutos);
+
+float segundos_para_horas(float segundos);
+
+float segundos_para_minutos(float segundos);
+
+float dias_para_horas(float dias);
+
+float horas_para_dias(float horas);
+
+void exibir_menu();
+
+void converter_tempo();
+
+#endif
